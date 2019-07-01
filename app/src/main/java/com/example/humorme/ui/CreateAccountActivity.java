f119fd6ea6e6b954136f1a1884bd4b24da46f2c9
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-07-01 12:44:55 +0300

    adds the login and signup activities with their styled xml views

---
+++ app/src/main/java/com/example/humorme/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.humorme.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.humorme.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
--- app/src/main/java/com/example/humorme/ui/LoginActivity.java
+++ app/src/main/java/com/example/humorme/ui/LoginActivity.java
@@ -1,14 +1,15 @@
-package com.example.humorme;
+package com.example.humorme.ui;
 
 import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.humorme.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
